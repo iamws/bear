92addf41de127454a404d9049f33881ec3871525
iamwsbear <8098814@qq.com>
2014-12-08 21:10:04 +0800

    网络增加监听

--- src/org/iamwsbear/weightpk/common/activity/UpdateActivity.java
+++ src/org/iamwsbear/weightpk/common/activity/UpdateActivity.java
@@ -1,11 +1,19 @@
 package org.iamwsbear.weightpk.common.activity;
 
+import org.iamwsbear.weightpk.R;
+import org.iamwsbear.weightpk.common.broadcast.NetWorkBroadCast;
+import org.iamwsbear.weightpk.common.broadcast.NetWorkBroadCast.NetWorkChangeListener;
+import org.iamwsbear.weightpk.util.NetWorkUtil;
+
 import android.app.Activity;
 import android.app.Dialog;
 import android.os.Bundle;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
-public class UpdateActivity extends Activity{
+public class UpdateActivity extends Activity implements NetWorkChangeListener{
+    
+    private static final String TAG = UpdateActivity.class.getName();
 	
 	private Dialog noticeDialog;
 	
@@ -14,6 +22,31 @@ public class UpdateActivity extends Activity{
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		NetWorkBroadCast.addNetWorkChangeListener(this);
 	}
+
+    @Override
+    public void onNetWorkStateChangeListener(int netType) {
+        switch (netType) {
+        case NetWorkUtil.NET_INVALID:
+            Toast.makeText(this, "短嗤利大", Toast.LENGTH_SHORT).show();
+            break;
+        case NetWorkUtil.NET_2G:
+            Toast.makeText(this, "2g利大", Toast.LENGTH_SHORT).show();
+            break;
+        case NetWorkUtil.NET_3G:
+            Toast.makeText(this, "3g利大", Toast.LENGTH_SHORT).show();
+            break;
+
+        default:
+            break;
+        }
+    }
+    
+    @Override
+    protected void onDestroy() {
+        NetWorkBroadCast.removeNetWorkChangeListener(this);
+        super.onDestroy();
+    }
 
 }
--- src/org/iamwsbear/weightpk/common/broadcast/NetWorkBroadCast.java
+++ src/org/iamwsbear/weightpk/common/broadcast/NetWorkBroadCast.java
@@ -1,23 +1,34 @@
 package org.iamwsbear.weightpk.common.broadcast;
 
 import java.lang.ref.WeakReference;
+import java.util.Iterator;
 import java.util.List;
 
 import org.iamwsbear.weightpk.util.NetWorkUtil;
 
-import com.google.common.collect.Lists;
-
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Handler;
 import android.util.Log;
 
+import com.google.common.collect.Lists;
+
+/**
+ * 注册网络变化通知广播
+ * 
+ * @author iamwsbear@gmail.com
+ *
+ */
+
 public class NetWorkBroadCast extends BroadcastReceiver {
-    
+
     private static final String TAG = NetWorkBroadCast.class.getName();
-    
+
     private static NetWorkBroadCast netWorkBroadCast;
     
+    private Handler handler = new Handler();
+
     public NetWorkBroadCast(Context context) {
         super();
     }
@@ -28,40 +39,95 @@ public class NetWorkBroadCast extends BroadcastReceiver {
         }
         return netWorkBroadCast;
     }
-    
+
     @Override
     public void onReceive(Context context, Intent intent) {
         Log.d(TAG, "THE NET WORK IS CHANGE");
-        Log.d(TAG,"the connect is "+NetWorkUtil.isNetworkConnected(context));
-        String notice="";
+        Log.d(TAG, "the connect is " + NetWorkUtil.isNetworkConnected(context));
+        String notice = "";
         switch (NetWorkUtil.getNetworkType(context)) {
         case NetWorkUtil.NET_INVALID:
-            notice ="无网络";
+            notice = "无网络";
             break;
         case NetWorkUtil.NET_WIFI:
-            notice ="WIFI------";
+            notice = "WIFI------";
             break;
         case NetWorkUtil.NET_2G:
-            notice ="2G-------";
+            notice = "2G-------";
             break;
         case NetWorkUtil.NET_3G:
-            notice ="3G----------";
+            notice = "3G----------";
             break;
         default:
             break;
         }
         Log.d(TAG, notice);
+        notifyAllListeners(NetWorkUtil.getNetworkType(context));
     }
-    
+
     /**
      * 定义监听接口用于通知用户网络变化的情况
+     * 
      * @author iamwsbear@gmail.com
      *
      */
-    public interface NetWorkChangeListener{
-        public void onNetWorkStateChangeListener();
+    public interface NetWorkChangeListener {
+        public void onNetWorkStateChangeListener(int netType);
+    }
+
+    private static List<WeakReference<NetWorkChangeListener>> netWorkListenerList = Lists
+            .newArrayList();
+
+    public static void addNetWorkChangeListener(NetWorkChangeListener o) {
+        if (o == null || netWorkListenerList == null) {
+            return;
+        } else {
+            synchronized (netWorkListenerList) {
+                for (WeakReference<NetWorkChangeListener> ref : netWorkListenerList) {
+                    if (o.equals(ref.get())) {
+                        return;
+                    }
+                }
+                WeakReference<NetWorkChangeListener> ref = new WeakReference<NetWorkChangeListener>(
+                        o);
+                netWorkListenerList.add(ref);
+            }
+        }
+    }
+
+    public static void removeNetWorkChangeListener(NetWorkChangeListener o) {
+        if (o == null || netWorkListenerList == null) {
+            return;
+        } else {
+            synchronized (netWorkListenerList) {
+                Iterator<WeakReference<NetWorkChangeListener>> iterator = netWorkListenerList
+                        .iterator();
+                while (iterator.hasNext()) {
+                    WeakReference<NetWorkChangeListener> ref = iterator.next();
+                    if (o.equals(ref.get())) {
+                        ref.clear();
+                        iterator.remove();
+                        return;
+                    }
+                }
+            }
+        }
     }
     
-    private static List<WeakReference<NetWorkChangeListener>> netWorkListeners = Lists.newArrayList();
+    private void notifyAllListeners(final int netType){
+        if(netWorkListenerList != null){
+            Iterator<WeakReference<NetWorkChangeListener>> iterator = netWorkListenerList.iterator();
+            while(iterator.hasNext()){
+                final WeakReference<NetWorkChangeListener> ref = iterator.next();
+                handler.post(new Runnable() {
+                    
+                    @Override
+                    public void run() {
+                         ref.get().onNetWorkStateChangeListener(netType);
+                    }
+                });
+            }
+        }
+    }
 
 }
